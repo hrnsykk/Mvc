3da8698309df185f159ca2c249f5f75521323261
Harun Soykok <h.soykok@gmail.com>
2019-09-05 12:25:06 +0200

    ilk commit

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 
 date_default_timezone_set('Europe/Amsterdam');
-define('URL', 'http://localhost/TurkseMarkt/');
+define('URL', 'http://localhost/TurkseMarktV2/');
 define('ASSETS',  URL . 'Assets/');
 define('AKTIFURL', $_SERVER['REQUEST_URI']);
 define('AKTIFURL2', $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
